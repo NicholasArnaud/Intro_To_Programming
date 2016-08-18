c6d1ff5b2548eb804f1ba857367b343130ceeb0e
NicholasArnaud <cobrahead09@gmail.com>
2016-08-18 16:02:39 -0500

    Variable Exercise
    Added work from Variable exercise sheet

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,4 +1,98 @@
 #include <iostream>
+
+/* Number 1
+EXAMPLE A
+int num = 2 / 3 + 3 / 5;
+printf("%i", num);
+system("pause");
+answer:0
+
+EXAMPLE B
+int num = 2 % 3 + 3 % 5;
+printf("%i",num);
+system("pause");
+answer:5
+
+EXAMPLE C
+int num = 23 % 15 % -2;
+printf("%i", num);
+system("pause");
+answer:0
+
+EXAMPLE D
+int num = 25 * 1 / 2;
+printf("%i",num);
+answer:12
+
+EXAMPLE E
+int num = 25 * 1.0 / 2;
+printf("%i", num);
+answer:12
+
+EXAMPLE F
+int num = 25 * (1 / 2);
+printf("%i", num);
+answer:0
+
+EXAMPLE G
+int num = 235 / 8 + 7;
+printf("%i", num);
+answer22
+
+EXAMPLE H
+int num = 235 / 8.0 + 7;
+printf("%i", num);
+answer:36
+
+EXAMPLE I
+int num = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+printf("%i", num);
+system("pause");
+answer:0
+*/
+/* Number 2
+EXAMPLE A
+Hieght
+
+EXAMPLE B
+Overincome
+
+EXAMPLE C
+Belowincome
+
+EXAMPLE D
+Emptywallet
+*/
+/* Number 3
+Example A
+int
+
+EXAMPLE B
+float
+
+EXAMPLE C
+Char
+
+EXAMPLE D
+int
+
+EXAMPLE E
+bool
+*/
+/*
+Number 4
+int first = 2;
+int sec = 3;
+int temp = 0;
+
+temp = first;
+first = sec;
+sec = temp;
+
+printf("%i", first);
+printf("%i", sec);
+*/
+
 int num = 25;
 int main()
 {
