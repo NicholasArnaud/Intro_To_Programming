f2aee0683f6aa1e613de711b00e2d5d110b4a096
NicholasArnaud <cobrahead09@gmail.com>
2016-08-24 17:59:47 -0500

    Added Array Exercise
    Started Array Exercise work

---
+++ Array Exercise/Project1/Project1/Source.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+int main()
+{
+
+	/*
+	Problem 1;
+
+	a. Valid
+	b. It is trying to declare 7 variables with only 5 spaces declared
+	c. Not enough declared variables
+	d. Too many comas in between declared variables
+	e. Valid
+	f. Have to declare an amount of space given
+	g. Arrays can not store negative space
+	i. Needs to have scope operators
+	*/
+
+	/*
+
+	//Problem 2
+		int value[] = { 2,6,10,14 };
+		std::cout << value[2] << std::endl;
+		// a. 10
+		std::cout << ++value[0]<<std::endl;
+		// b. 3
+		std::cout << value[1]++ << std::endl;
+		// c. 6
+		int x = 2;
+		std::cout << value[++x]<<std::endl;
+		// d. 14
+		std::cout << value[4] << std::endl;
+		// e. Error
+
+
+	//Problem 3
+
+		int data[10];
+		for (int data = 10; data > 0; data--)
+			std::cout << data << std::endl;
+
+		
+		//Problem 4
+	int arr[5];
+	int i;
+	std::cin >> arr[0];
+	std::cin >> arr[1];
+	std::cin >> arr[2];
+	std::cin >> arr[3];
+	std::cin >> arr[4];
+	std:: cout<< std:: endl;
+	for (int i = 4; i >= 0; i--)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+	*/
+
+	//Problem 5
+	int arr[10];
+	int i;
+	for (i=9; i >= 0; i--)
+	{
+		std::cin >> arr[i];
+	}
+	if (arr[i] > arr[i])
+
+	//Problem 6
+		int Arr[3][3];
+
+
+
+
+
+	std::cout << std::endl;
+	system("pause");
+}
