593cbb1688bcbb1770ddfd88a8a05cc106e3ee1a
NicholasArnaud <cobrahead09@gmail.com>
2016-08-28 00:00:55 -0500

    Array Exercise
    Added an answer and made a fix to function exercise

--- Array Exercise/Project1/Project1/Source.cpp
+++ Array Exercise/Project1/Project1/Source.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 int main()
 {
-
-	/*
-	Problem 1;
-
+//Problem 1
+/*	//Answer
 	a. Valid
 	b. It is trying to declare 7 variables with only 5 spaces declared
 	c. Not enough declared variables
@@ -13,11 +11,10 @@ int main()
 	f. Have to declare an amount of space given
 	g. Arrays can not store negative space
 	i. Needs to have scope operators
-	*/
+*/
 
-	/*
-
-	//Problem 2
+//Problem 2
+/*	//Answer
 		int value[] = { 2,6,10,14 };
 		std::cout << value[2] << std::endl;
 		// a. 10
@@ -30,78 +27,147 @@ int main()
 		// d. 14
 		std::cout << value[4] << std::endl;
 		// e. Error
+	*/
 
+//Problem 3
+/*	//Answer
+int data[10];
+for (int data = 10; data > 0; data--)
+std::cout << data << std::endl;
+*/
 
-	//Problem 3
-
-		int data[10];
-		for (int data = 10; data > 0; data--)
-			std::cout << data << std::endl;
-
-		
-		//Problem 4
-		/*
-		Create a program that asks for 5 numbers from the user,
-		and store them in an array. 
-		Print out the numbers in reverse order
-		*/
+//Problem 4
 /*
-	int arr[5];
-	int i;
-	std::cin >> arr[0];
-	std::cin >> arr[1];
-	std::cin >> arr[2];
-	std::cin >> arr[3];
-	std::cin >> arr[4];
-	std:: cout<< std:: endl;
-	for (int i = 4; i >= 0; i--)
-	{
-		std::cout << arr[i] << std::endl;
+	Create a program that asks for 5 numbers from the user,
+	and store them in an array.
+	Print out the numbers in reverse order
+*/
+/*	//Answer
+		int arr[5];
+		int i;
+		std::cin >> arr[0];
+		std::cin >> arr[1];
+		std::cin >> arr[2];
+		std::cin >> arr[3];
+		std::cin >> arr[4];
+		std:: cout<< std:: endl;
+		for (int i = 4; i >= 0; i--)
+		{
+	std::cout << arr[i] << std::endl;
 	}
-	*/
+*/
 
-	//Problem 5
-	/*
-	Write a program that lets the user enter 10 values into an array. 
-	The program should then display the largest 
-	and smallest values stored in the array.
-	*/
-	/*int arr[10];
-	int i;
-	for (i=9; i >= 0; i--)
-	{
-		std::cin >> arr[i];
-	}
-	if (arr[i] )
-	*/
-	//Problem 6
-	/*
+//Problem 5
+/*
+			Write a program that lets the user enter 10 values into an array.
+			The program should then display the largest
+			and smallest values stored in the array.
+			*/
+/*int arr[10];
+			int i;
+			for (i=9; i >= 0; i--)
+			{
+				std::cin >> arr[i];
+			}
+			if (arr[i] )
+			*/
+
+//Problem 6
+/*
 	Create a program that creates a small 3x3 array of integers.
 	Use a nested for loop to initialise the numbers 1-9.
 	Output the numbers in a grid format.
-	*/
+*/
+/* //Answer
 	int Arr[3][3];
 	int number = 1;
 	for (int numA=0; numA < 3;numA++)
 	{
 		std::cout <<std:: endl;
 		for (int numB = 0; numB < 3; numB++)
-		{ 
-			Arr[numA][numB] = number;
-			number++;
-			std::cout << Arr[numA][numB];
+		{
+		Arr[numA][numB] = number;
+		number++;
+		std::cout << Arr[numA][numB];
 		}
 	}
-	
-	//Problem 7
-	/*
-	
-	An application uses a two dimensional array defined as follows:
+*/
+
+//Problem 7
+/*
+
+An application uses a two dimensional array defined as follows:
 int days[29][5];
 Write code that sums each row in the array and displays the results.
 Write code that sums each column in the array and displays the results.
 */
+/* //Answer
+int Days[29][5], colTotal[29] = { 0 }, rowTotal[5] = { 0 };
+int x, y;
+std::cout << "All array inputs for each is 5" << std::endl;
+for (x = 0; x < 29; x++)
+	for (y = 0; y < 5; y++)
 
-	std::cout << std::endl;
-	system("pause");
+	{
+
+		Days[x][y] = 5;
+	}
+
+for (x = 0; x < 29; ++x)
+{
+	for (y = 0; y < 5; ++y)
+	{
+		colTotal[x] = colTotal[x] + Days[x][y];
+	}
 }
+for (x = 0; x < 29; x++)
+{
+	std::cout << colTotal[x] << std::endl;
+}
+
+
+for (x = 0; x < 29; x++)
+{
+	for (y = 0; y < 5; y++)
+	{
+		rowTotal[y] = rowTotal[y] + Days[x][y];
+	}
+}
+for (y = 0; y < 5; y++)
+{
+	std::cout << "sum of col is " << rowTotal[y] << std::endl;
+}
+*/
+
+//Problem 8
+/*
+Create a program that declares an array of 5 integer values. These values will represent the
+health of 5 characters. Initialise the values all to 100. Your program is to ask the user to
+enter a number (0 to 4) to select a character that is to be attacked by a monster. Each attack
+deals 40 damage per attack. If a player’s health gets below 0 then should not be able to be
+attacked. Your program should simulate 5 attacks. After the attacks have finished, output
+the parties’ remaining health to the console.
+CHALLENGE: Research random number generation and modify your program so that random
+damage is dealt with each attack.
+
+	int Players[5];
+		int Health;
+		*/
+
+//Problem 9
+/*
+		Daenerys Targaryen wants to keep track of how many kilos of food her three dragons eat
+		each day during a typical week. Write a program that stores this information in a 2
+		dimensional 3 x 7 array, where each row represents a different dragon and each column
+		represents a different day of the week. The program should first have Dany input the user
+		for each dragon. Then it should create a report that includes the following information:
+		- Average amount of food eaten per day by all the dragons together
+		- average amount of food eaten per day by any one dragon
+		- the greatest amount of food eaten during one day and which dragon that was
+		- the least amount of food eaten during one day and which dragon that was
+		*/
+
+std::cout << std::endl;
+system("pause");
+}
+	
--- Functions Exercise/Functions Exercise/Source.cpp
+++ Functions Exercise/Functions Exercise/Source.cpp
@@ -9,6 +9,9 @@ int i;
 
 
 //Problem 1
+/*
+What is the output of this program?
+*/
 void Printer(int variable)
 {
 	std::cout << variable << std::endl;
@@ -66,7 +69,11 @@ std::cout << std::endl;
 		}
 	}
 
-//Problem 3 Incomplete
+//Problem 3 
+/*
+Add another function with the same name as the one you wrote in question 2, but with
+three floats instead of two. What is this language feature called?
+*/
 
 
 
@@ -110,8 +117,8 @@ int CoinToss()
 
 
 //Problem 6
-
-/*Find the error in each of the following functions 
+/*
+Find the error in each of the following functions 
 and explain how to fix them.
 */
 
@@ -131,7 +138,6 @@ int sum(int n) //needs ";"
 		n = n + n;
 }
 
-#include <iostream>
 int main()
 {
 	double x = 13.6;
@@ -146,25 +152,154 @@ int square(int x) //change "int square(int x)" to "float square(int x)
 }
 
 //Problem 7
-
-#include <iostream>
-for SumTo(int N = 0; N >= 1; N++)
-{
-
-}
+/*
+Write a function called SumTo that accepts an integer parameter N and returns the sum of
+all integers from 1 to N, including N.
+Use this code to test the function:#include <iostream>
+//Your function called SumTo goes here
 int main()
 {
+int result = SumTo(3); //result = 1 + 2 + 3 = 6
+std::cout << result << std::endl;
+result = SumTo(15); //result should now be 120
+std::cout << result << std::endl;
+}
+*/
+
+//Problem 8
+/*
+Write a function that takes as its parameters an array of integers and the size of the array
+and returns the sum of the values in the array.
+Use this code to test the function:
+#include <iostream>
+//Your function called SumArray goes here
+int main()
+{
+int integer_array[5] = {7, 3, 2, 4, 9};
+int result = SumArray(integer_array, 5); //result = 25
+std::cout << result << std::endl;
+}
+*/
+
+//Problem 9
+/*
+Write a function that takes as its parameter an array of integers and the size of the array
+and returns the minimum of the values in the array.
+Use this code to test the function:#include <iostream>
+//Your function called MinInArray goes here
+int main()
+{
+int integer_array[7] = {10, 15, 7, 4, 13, 19, 8};
+int result = MinInArray(integer_array, 7); //result = 4
+std::cout << result << std::endl;
+}
+*/
+
+//Problem 10
+/*
+Write a function that takes as its parameter an array called input_array of integers, the
+array size and a second array of the same size called output_array. Fill each element in the
+second array to be the value in the same index of input_array multiplied by its index in the
+array.
+Use this code to test your function:#include <iostream>
+//Your function called MultiplyByIndex goes here
+int main()
+{
+int integer_array[7] = {10, 15, 7, 4, 13, 19, 8};
+int output_array[7] = {};
+MultiplyByIndex(integer_array, output_array, 7);
+//output_array should be {0, 15, 14, 12, 52, 95, 48};
+return 0;
+}
+*/
+
+//Problem 11
+/*
+Write a function that takes as its parameters two input arrays of integers , an integer for
+their size and an output array. Set the value at each index to the sum of the corresponding
+two elements of the input arrays at the same index. Assume the three arrays are of equal
+length. Write your own code for testing this function.
+*/
+
+//Problem 12
+/*
+Write a function that takes as its parameters an array called array_input of integers and the
+size of the array and modifies the given array so that it contains a running sum of its
+original values. For example, if the array originally had the values {3,2,4,7}, after running
+your function that array would instead contain {3,5,9,16}, and if you ran it another time
+passing the modified array in again, you'd have {3,8,17,33}. Write your own code for testing
+this function.
+*/
+
+//Problem 13
+/*
+Write a function that searches for a particular number in an array. The function should
+have three parameters: the array, the array size, and the number to be found. If the
+number is in the array, the function should return the position of the number in the array.
+If the number isn’t found, the function should return -1. In the case that the desired
+number appears more than once in the array, the function should return the position of
+the first occurrence. Write your own code to test this function.
+*/
+
+//Problem 14
+/*
+Write a function named Split that accepts one input array of integers, an integer for the
+size of the input array, and two output arrays. All numbers in the input array that are
+positive are copied into the first output array and all numbers in the input array that are
+negative are copied into the second output array. The function should return how many
+numbers were copied into the first output array. Write your own code to test this function.
+*/
+
+//Problem 15
+/*
+Write a function that calculates and then returns x to the power of y.
+*/
+
+//Problem 16
+/*
+Write a function that takes in an array of integers, and the size of the array. The function
+should print out the “look and say” sequence for the array. The look and say sequence
+works by printing out how many of the same number there are in a row followed by that
+number.
+For example, if the array was {1, 1, 1, 1} the function would print out “4, 1” because there
+are four ones.
+If the array was {1, 2, 2, 2, 3, 4} the function would print out “1,1,3,2,1,3,1,4” because there
+is “one 1, three 2s, one 3, one 4”.
+If the array is {1,2,2,1,5,1,1,7,7,7,7,1,1,1,1,1,1,1,1} the function should print out
+“1,1,2,2,1,1,1,5,2,1,4,7,8,1”
+Write your own code to test the function.
+*/
+
+//Problem 17
+/*
+Write a program that lets the user play the game of Rock, Paper, Scissors against the
+computer. The program should work as follows:
+1. When the program begins, a random number in the range of 1 - 3 is generated. If
+the number is 1, then the computer has chosen rock. If the number is 2, then the
+computer has chosen paper. So if the number is 3 then the computer has chosen
+scissors.
+2. The user enters their choice of “rock”, “paper” or “scissors”
+3. The computer’s choice is displayed.
+4. A winner is selected according to the following rules:
+1. Rock beats scissors, scissors beats paper, paper beats rock
+2. If there is a tie then the game must be replayed
+5. Make sure to break the game up into functions to perform each task. Make sure to
+look for any code that is repeated, and break it out into a function.
+*/
+
+
+
+
+int main()
+{
+	//part of 7
 	int result = SumTo(3); //result = 1 + 2 + 3 = 6
 	std::cout << result << std::endl;
 	result = SumTo(15); //result should now be 120
 	std::cout << result << std::endl;
-}
 
 
 
-
-int main()
-{
 	float stuff = wordNow();
 	float more = Half(16.721);
 	std::cin >> Rand;
