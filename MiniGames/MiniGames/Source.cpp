ea6a9d024d84309c71a2a2728e3330edd1e341ac
NicholasArnaud <cobrahead09@gmail.com>
2016-09-07 18:02:30 -0500

    Homework
    Changed format of tic tac toe game

--- MiniGames/MiniGames/Source.cpp
+++ MiniGames/MiniGames/Source.cpp
@@ -8,9 +8,9 @@ int main()
 {
 	char input;
 	int rand();
-	std::cout << "Press 'm' for menu"<< std:: endl;
+	std::cout << "Press 'm' for menu" << std::endl;
 	std::cin >> input;
-	
+
 	if (input == 'm')
 	{
 		std::cout << "Press 'a' to play Rock, Paper, Scissers \n";
@@ -20,9 +20,10 @@ int main()
 		std::cout << "Press 'q' to quit playig \n";
 		system("pause");
 	}
+
 	std::cin >> input;
 	// rock paper scissors
-	if (input == 'a'&& input !='q')
+	if (input == 'a'&& input != 'q')
 	{
 		do {
 
@@ -159,17 +160,17 @@ int main()
 
 
 	// dice roll
-	if (input =='c' && input !='q')
+	if (input == 'c' && input != 'q')
 	{
-	int computer = 0;
-	bool complete = false;
-	computer = (rand() + time(0)) % 6;
-	int dice = (rand() + time(0)) % 6;
+		int computer = 0;
+		bool complete = false;
+		computer = (rand() + time(0)) % 6;
+		int dice = (rand() + time(0)) % 6;
 		while (!complete)
 		{
 			std::cout << "Press '1' to roll die." << std::endl;
 			std::cin >> dice;
-		
+
 			if (dice == computer)
 			{
 				std::cout << "Tie" << std::endl;
@@ -187,210 +188,140 @@ int main()
 					std::cout << "Try again?" << std::endl;
 				}
 		}
-	}	
+	}
 
 
 	// Tic-Tac-Toe
-	if (input == 'd' && input !='q')
-	{
-		
-		int checkwin(char[]);
-		void board(char[]);
-		char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
-		int player = 1, i, choice;
+	//void showBoard();
+	//void player_turn();
+	//bool gameover();
 
-		char mark;
-		do
-		{
-			board(square);
+	//char turn;
+	//bool draw = false;
+	//char board[3][3] = { { '1', '2', '3' },{ '4', '5', '6' },{ '7', '8', '9' } };
 
-			if (player % 2 == 1)
-				player = 1;
-			else
-				player = 2;
+	//int main()
+	//{
+	//	std::cout << "Tic Tac Toe Game\n";
+	//	std::cout << "Player 1 [X] --- Player 2 [O]\n";
+	//	turn = 'X';
 
+	//	while (!gameover())
+	//	{
+	//		showBoard();
+	//		player_turn();
+	//		gameover();
+	//	}
 
-			// player 2
-			if (player == 2)
-			{
-				std::cout << "Players  " << player << std::endl;
-				std::cout << "Press Enter for Computer" << std::endl;
-				choice = rand() % 9;
-				mark = 'O';
-				int turn = 1;
-				int placed = 0;
-				while (placed == 0)
-				{
+	//	if (turn == 'O' && !draw)
+	//	{
+	//		showBoard();
+	//		std::cout << std::endl << std::endl << "Player 1 [X] Wins! Game Over!\n";
+	//	}
+	//	else if (turn == 'X' && !draw)
+	//	{
+	//		showBoard();
+	//		std::cout << std::endl << std::endl << "Player 2 [O] Wins! Game Over!\n";
+	//	}
+	//	else
+	//	{
+	//		showBoard();
+	//		std::cout << std::endl << std::endl << "It's a draw! Game Over!\n";
+	//	}
+	//	system("pause");
+	//}
 
-					if (choice == 1 && square[1] == '1') {
-						square[1] = mark;
-						placed = 1;
-					}
-					else if (choice == 2 && square[2] == '2') {
+	//void showBoard()
+	//{
+	//	std::cout << "---------------------" << std::endl << std::endl;
+	//	std::cout << "     |     |     " << std::endl;
+	//	std::cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << std::endl;
+	//	std::cout << "_____|_____|_____" << std::endl;
+	//	std::cout << "     |     |     " << std::endl;
+	//	std::cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << std::endl;
+	//	std::cout << "_____|_____|_____" << std::endl;
+	//	std::cout << "     |     |     " << std::endl;
+	//	std::cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << std::endl;
+	//	std::cout << "     |     |     " << std::endl;
+	//}
 
-						square[2] = mark;
-						placed = 1;
-					}
-					else if (choice == 3 && square[3] == '3') {
+	//void player_turn()
+	//{
+	//	int choice;
+	//	int row = 0, column = 0;
 
-						square[3] = mark;
-						placed = 1;
-					}
-					else if (choice == 4 && square[4] == '4') {
-
-						placed = 1;
-						square[4] = mark;
-					}
-					else if (choice == 5 && square[5] == '5') {
-
-						square[5] = mark;
-						placed = 1;
-					}
-					else if (choice == 6 && square[6] == '6') {
-
-						square[6] = mark;
-						placed = 1;
-					}
-					else if (choice == 7 && square[7] == '7') {
-
-						square[7] = mark;
-						placed = 1;
-					}
-					else if (choice == 8 && square[8] == '8') {
-
-						square[8] = mark;
-						placed = 1;
-					}
-					else if (choice == 9 && square[9] == '9') {
-
-						square[9] = mark;
-						placed = 1;
-					}
-
-					else
-					{
-						choice = rand() % 9;
-					}
-				}
-				i = checkwin(square);
-				player++;
-				std::cin.get();
-				board(square);
-			}
-
-			// player 1
-			else if (player == 1)
-			{
-				std::cout << "Players  " << player << ", enter a number:  ";
-				std::cin >> choice;
-				mark = 'X';
-
-				if (choice == 1 && square[1] == '1')
-
-					square[1] = mark;
-				else if (choice == 2 && square[2] == '2')
-
-					square[2] = mark;
-				else if (choice == 3 && square[3] == '3')
-
-					square[3] = mark;
-				else if (choice == 4 && square[4] == '4')
-
-					square[4] = mark;
-				else if (choice == 5 && square[5] == '5')
-
-					square[5] = mark;
-				else if (choice == 6 && square[6] == '6')
-
-					square[6] = mark;
-				else if (choice == 7 && square[7] == '7')
-
-					square[7] = mark;
-				else if (choice == 8 && square[8] == '8')
-
-					square[8] = mark;
-				else if (choice == 9 && square[9] == '9')
-
-					square[9] = mark;
-				else
-				{
-					std::cout << "Invalid move ";
-
-					player--;
-					std::cin.get();
-				}
-				i = checkwin(square);
-
-				player++;
-			}
-		} while (i == -1);
-		board(square);
-		if (i == 1)
-
-			std::cout << "Congratulation! \nPlayer " << --player << " win ";
-		else
-			std::cout << "  OOps!\nGame draw";
-
-		std::cin.get();
+	//	if (turn == 'X')
+	//	{
+	//		std::cout << "Player 1 turn [X]: ";
+	//		std::cin >> choice;
+	//	}
+	//	else if (turn == 'O')
+	//	{
+	//		std::cout << "Player 2 turn [O]: ";
+	//		srand(time(NULL));
+	//		choice = rand() % 9 + 1;
+	//		srand(1);
+	//	}
 
 
-		void board(char square[])
-			; {
-			system("cls");
-			std::cout << "\n\n\tTic Tac Toe\n\n";
+	//	switch (choice)
+	//	{
+	//	case 1: row = 0; column = 0; break;
+	//	case 2: row = 0; column = 1; break;
+	//	case 3: row = 0; column = 2; break;
+	//	case 4: row = 1; column = 0; break;
+	//	case 5: row = 1; column = 1; break;
+	//	case 6: row = 1; column = 2; break;
+	//	case 7: row = 2; column = 0; break;
+	//	case 8: row = 2; column = 1; break;
+	//	case 9: row = 2; column = 2; break;
+	//	default:
+	//		std::cout << "You didn't enter a correct number! Try again\n";
+	//		player_turn();
+	//	}
 
-			std::cout << "Player 1 (X)  -  Player 2 (O)" << std::endl << std::endl;
-			std::cout << std::endl;
+	//	if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O')
+	//	{
+	//		board[row][column] = 'X';
+	//		turn = 'O';
+	//	}
+	//	else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O')
+	//	{
+	//		board[row][column] = 'O';
+	//		turn = 'X';
+	//	}
+	//	else
+	//	{
+	//		std::cout << "The cell you chose is used! Try again\n";
+	//		player_turn();
+	//	}
 
-			std::cout << "     |     |     " << std::endl;
-			std::cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << std::endl;
+	//}
 
-			std::cout << "_____|_____|_____" << std::endl;
-			std::cout << "     |     |     " << std::endl;
+	//bool gameover()
+	//{
+	//	for (int i = 0; i < 3; i++)//Check for a win
+	//	{
+	//		if ((board[i][0] == board[i][1] && board[i][1] == board[i][2]) || (board[0][i] == board[1][i] && board[1][i] == board[2][i]) || (board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] == board[1][1] && board[1][1] == board[2][0]))
+	//		{
+	//			return true;
 
-			std::cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << std::endl;
+	//		}
+	//	}
 
-			std::cout << "_____|_____|_____" << std::endl;
-			std::cout << "     |     |     " << std::endl;
+	//	for (int i = 0; i < 3; i++)//Check for draw
+	//	{
+	//		for (int j = 0; j < 3; j++)
+	//		{
+	//			if (board[i][j] != 'X' && board[i][j] != 'O')
+	//			{
 
-			std::cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << std::endl;
+	//				return false;
 
-			std::cout << "     |     |     " << std::endl << std::endl;
-		}
+	//			}
+	//		}
+	//	}
+	//	draw = true;
+	//	return true;
 
-
-		int checkwin(char square[])
-			;
-		{
-		if (square[1] == square[2] && square[2] == square[3])
-
-			return 1;
-		else if (square[4] == square[5] && square[5] == square[6])
-
-			return 1;
-		else if (square[7] == square[8] && square[8] == square[9])
-
-			return 1;
-		else if (square[1] == square[4] && square[4] == square[7])
-
-			return 1;
-		else if (square[2] == square[5] && square[5] == square[8])
-
-			return 1;
-		else if (square[3] == square[6] && square[6] == square[9])
-
-			return 1;
-		else if (square[1] == square[5] && square[5] == square[9])
-			return 1;
-		else if (square[3] == square[5] && square[5] == square[7])
-
-			return 1;
-		else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-			&& square[4] != '4' && square[5] != '5' && square[6] != '6'
-			&& square[7] != '7' && square[8] != '8' && square[9] != '9')
-
-			return 0;
-		else
-			return -1;
-	}
-	}
+	//}
