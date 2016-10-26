09176063a9d1af570536ef69e1fbfe3feaff4d82
NicholasArnaud <cobrahead09@gmail.com>
2016-10-26 17:52:56 -0500

    Template excerice progress

--- Template Exercise/Template Exercise/Source.cpp
+++ Template Exercise/Template Exercise/Source.cpp
@@ -29,7 +29,8 @@ A Min(A a, A b)
 Max
 DESCRIPTION: Returns the higher of two values.
 INPUT: Two template arguments (a, b).
-OUTPUT: The higher of the two values a and b.*/
+OUTPUT: The higher of the two values a and b.
+*/
 template <typename A>
 A Max(A a, A b)
 {
@@ -66,12 +67,20 @@ INPUT: Two template arguments (a, b).
 OUTPUT: The alphabetically lower of the two values a and b
 */
 template <typename T>
-T Max(T a, T b)
+T Maxt(T a, T b)
 {
-	
-
-
-	return ;
+	if (a < b)
+	{
+		return a < b;
+	}
+	else if (a > b)
+	{
+		return b < a;
+	}
+	else
+	{
+		return a;
+	}
 }
 
 
@@ -83,12 +92,103 @@ INPUT: Two template arguments (a, b).
 OUTPUT: The alphabetically higher of the two values a and b.
 */
 template <typename T>
-T Min(T a, T b)
+T Mint(T a, T b)
 {
-	
-	return ;
+	if (a < b)
+	{
+		return a < b;
+	}
+	else if (a > b)
+	{
+		return b < a;
+	}
+	else
+	{
+		return a;
+	}
 }
 
+
+
+
+
+
+
+
+template <typename Template>
+class Arrays
+{
+	/*
+Constructor
+DESCRIPTION: Creates an array on the heap of the specified capacity. The new heap
+memory should be zeroed using memset. You should store the value of
+capacity and size for later use (size should start at 0).
+INPUT: A single integer specifying the capacity for the data array.
+OUTPUT: N/A.
+*/
+	Template *Array;
+	int index;
+public:
+	
+	template <typename Template>
+	Arrays() 
+	{
+		Array[12];
+		index = 0;
+	};
+	
+/*
+Deconstructor
+DESCRIPTION: Deletes the data pointed to on the heap.
+INPUT: N/A.
+OUTPUT: N/A.
+*/
+
+~Arrays() {};
+
+/*
+Add
+DESCRIPTION: Adds an item to the next empty array location (you should be able to use the
+size value as an index to the next free location). Before adding the new item
+to the array, we should check if there is room and call Expand if not.
+INPUT: One template arguments (item).
+OUTPUT: None.
+*/
+	template <typename Template>
+	void Add(Template item)
+	{
+		if (Array[Max - 1] != '\0')
+		{
+			Array[index] = item;
+			index++;
+
+		}
+		else
+		{
+			Expand();
+		}
+	}
+/*
+Expand
+DESCRIPTION: Doubles the capacity of the data array by creating a new array on the heap
+with twice the current capacity, and then copying the data from the current
+array into the new array using memcpy. (Be sure to delete the old memory!)
+INPUT: None.
+OUTPUT: None.
+*/
+	template <typename Template>
+	void Expand()
+	{
+		Array[Max * 2];
+	}
+/*
+Operator []
+DESCRIPTION: Returns an item from the array at a specified index (like a regular subscript).
+INPUT: A single integer specifying the array-index of the item to return.
+OUTPUT: A copy/reference (your choice) of an item from the data array.
+*/
+
+};
 
 
 
@@ -98,7 +198,8 @@ int main()
 	Min<int>(12, 10);
 	Max <int>(15, 11);
 	Clamp<int>(0 ,0, 0);
-	Min<char>('l', 'm');
-	Max<char>('j', 'z');
+	Mint<char>('l', 'm');
+	Maxt<char>('j', 'z');
+	
 	return 1;
 }
